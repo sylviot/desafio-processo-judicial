11d29d6e685e56b217b48b7c054f1fe0629308f5
sylviot <sylvio.tavares@hotmail.com>
2021-06-05 16:07:14 -0400

    added: database SqlServer com EFCore Context

---
+++ api/Infra/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Infra
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options) { }
+    }
+}
