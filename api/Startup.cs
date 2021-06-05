04b3887d95b5031205d0c87e862b661044c9d630
sylviot <sylvio.tavares@hotmail.com>
2021-06-05 16:08:43 -0400

    fix: exibir swagger em todos ambientes

--- api/Startup.cs
+++ api/Startup.cs
@@ -43,9 +43,10 @@ namespace api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "api v1"));
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "api v1"));
 
             app.UseHttpsRedirection();
 
