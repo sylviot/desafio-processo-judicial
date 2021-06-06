6de299c46a48e05917233c4742e75a36650d1d9d
sylviot <sylvio.tavares@hotmail.com>
2021-06-06 14:23:32 -0400

    fix: adicionado campo ProcessoPai

--- api/Models/Processo.cs
+++ api/Models/Processo.cs
@@ -5,6 +5,7 @@ namespace api.Models
 {
     public class Processo : ModelBase
     {
+        public int? ProcessoPai { get; set; }
         public string NumeroUnificado { get; set; }
         public DateTime DataDistribuicao { get; set; }
         public bool SegredoJustica { get; set; }
